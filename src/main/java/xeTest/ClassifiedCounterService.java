b2cdd0affce14e3b0dcc7ebb1e06bc893d7b67f9
George Farantos <george.farantos@eurodyn.com>
2019-10-23 11:59:21 +0300

    Separate Service and Service implementation

--- src/main/java/xeTest/ClassifiedCounterService.java
+++ src/main/java/xeTest/ClassifiedCounterService.java
@@ -7,38 +7,8 @@ import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Service;
 
-@Service
-public class ClassifiedCounterService {
+public interface ClassifiedCounterService {
 
-	public ClassifiedCounterService() {
-		// empty constructor
-	}
+	public Classified processClassified(Classified classified);
 
-	public Classified processClassified(Classified classified) {
-		classified.setNumOfWords(count(classified.getText()));
-		classified.setAmount(findPrice(classified.getText()));
-
-		return classified;
-	}
-
-	private int count(String text) {
-		List<String> wordList = Arrays.asList(text.split(" "));
-		int count = 0;
-		for (String word : wordList) {
-			if (word.length() > 1) {
-				count++;
-			}
-		}
-		return count;
-	}
-
-	private String findPrice(String text) {
-		Pattern p = Pattern.compile("(\\d{1,3}(.\\d{3})*(,\\d+)?)(\\p{Sc}| *ÎµÏ…Ï�Ï‰| *euro)");
-		Matcher matcher = p.matcher(text);
-		if (matcher.find()) {
-			return matcher.group();
-		}
-
-		return "";
-	}
 }
--- src/test/java/xeTest/ClassifiedCounterServiceTest.java
+++ src/test/java/xeTest/ClassifiedCounterServiceTest.java
@@ -12,7 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes=ClassifiedCounterService.class)
+@SpringBootTest
 public class ClassifiedCounterServiceTest {
 
 	@Autowired
