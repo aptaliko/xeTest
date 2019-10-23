d0340c5ae279afd41c1f825ee87f72125906faba
George Farantos <george.farantos@eurodyn.com>
2019-10-23 13:17:15 +0300

    Removed unused import

--- src/main/java/classifiedcounter/services/ClassifiedCounterService.java
+++ src/main/java/classifiedcounter/services/ClassifiedCounterService.java
@@ -1,12 +1,5 @@
 package classifiedcounter.services;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.springframework.stereotype.Service;
-
 public interface ClassifiedCounterService {
 
 	public Classified processClassified(Classified classified);
